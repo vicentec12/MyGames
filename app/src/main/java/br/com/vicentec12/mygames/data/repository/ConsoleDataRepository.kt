16303b0d0d7ec169626758bbaa17a7d3619a4b84
Vicente de Castro <vicente_c39@hotmail.com>
2022-10-19 08:39:18 -0300

    Create Result map function and apply, and bug corrections

--- app/src/main/java/br/com/vicentec12/mygames/data/repository/ConsoleDataRepository.kt
+++ app/src/main/java/br/com/vicentec12/mygames/data/repository/ConsoleDataRepository.kt
@@ -1,27 +1,23 @@
 package br.com.vicentec12.mygames.data.repository
 
-import br.com.vicentec12.mygames.data.Result
-import br.com.vicentec12.mygames.di.Local
 import br.com.vicentec12.mygames.data.mapper.toEntityList
 import br.com.vicentec12.mygames.data.mapper.toModelList
 import br.com.vicentec12.mygames.data.source.ConsoleDataSource
+import br.com.vicentec12.mygames.di.Local
 import br.com.vicentec12.mygames.domain.model.Console
 import br.com.vicentec12.mygames.domain.repository.ConsoleRepository
+import br.com.vicentec12.mygames.extensions.map
 import javax.inject.Inject
 
 class ConsoleDataRepository @Inject constructor(
     @Local val consoleLocalDataSource: ConsoleDataSource
 ) : ConsoleRepository {
 
-    override suspend fun list() = when (val result = consoleLocalDataSource.list()) {
-        is Result.Success -> Result.Success(result.data?.toModelList().orEmpty(), result.message)
-        is Result.Error -> result
-    }
+    override suspend fun list() =
+        consoleLocalDataSource.list().map { data -> data?.toModelList().orEmpty() }
 
-    override suspend fun listWithGames() = when (val result = consoleLocalDataSource.listWithGames()) {
-            is Result.Success -> Result.Success(result.data?.toModelList().orEmpty(), result.message)
-            is Result.Error -> result
-        }
+    override suspend fun listWithGames() =
+        consoleLocalDataSource.listWithGames().map { data -> data?.toModelList().orEmpty() }
 
     override suspend fun insert(mConsoles: List<Console>) =
         consoleLocalDataSource.insert(mConsoles.toEntityList())
